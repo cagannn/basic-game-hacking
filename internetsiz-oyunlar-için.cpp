7db7df2b604a6080465b2f879b8839cbd1de222c
Çağan <cagangdk6@hotmail.com>
2020-04-11 05:41:39 +0300

    Create internetsiz-oyunlar-için.cpp

---
+++ internetsiz-oyunlar-için.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+DWORD processID;
+DWORD //değiştirmek-istediğiniz-şey(mermi can vb.)= 0x(adres numarası);
+
+int neye-çevirmek-istediğiniz = çevirilecek-sayı//(örn: int ammo=200;)
+int main() {
+	HWND hwnd = FindWindowA(NULL, "//oyunun adı");
+	
+	if (hwnd == NULL) {
+		cout << "Yok";
+	}
+	else {
+		GetWindowThreadProcessId(hwnd, &processID);
+		HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+		WriteProcessMemory(pHandle, (LPVOID)değiştirmek-istediğiniz-şey , &neye-çevirmek-istediğiniz, sizeof(neye-çevirmek-istediğiniz), 0);
+		
+	}
+
+	system("pause");
+  }
